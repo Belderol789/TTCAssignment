fee4c93be89101904860984095ff8f30ad503e9b
Kem Belderol <kemuel.belderol@mnltechnology.com>
2020-08-21 17:20:15 +0800

    Initial Commit

---
+++ TTCAssignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TTCAssignment
+//
+//  Created by Kem Belderol on 8/21/20.
+//  Copyright © 2020 Krats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
